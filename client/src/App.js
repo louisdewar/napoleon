6002508b2b7cc02a9a80f719f20a573c8e4fa145
Daulet <batayev.d@gmail.com>
2020-07-14 16:11:20 +0100

    Landing.js Basic Implementation + Message Handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
+import Landing from 'components/Landing';
 
 function App() {
-  const is_connected = useSelector(state => state.connected);
-  const user_id = useSelector(state => state.userID);
-  const room = useSelector(state => state.room);
-  const websocket = useSelector(state => state.socket);
-
-  const create_room = useCallback(() => {
-    websocket.createRoom('TEMP_USER_' + user_id);
-  }, [websocket, user_id]);
-
-  if (is_connected) {
-    if (!room) {
-      return (
-        <div className="App">
-          Connected our id is {user_id}.
-          <button onClick={create_room}>Create room</button>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          Connected our id is {user_id} and we are in room {room.key}. There are
-          {Object.keys(room.users).length} users connected.
-        </div>
-      );
-    }
-  } else {
-    return <div className="App">Not connected.</div>;
+  const room = useSelector(room);
+  if (room) {
+    return (<h1>You are in room {room.key}</h1>);
+  } else{
+    return (<Landing />);
   }
 }
 
